6c095a22cb1d926db8b3696d860922e68775366b
andravalciu <valciuandra@yahoo.com>
2026-02-21 01:07:06 +0200

    Add shadcn button component and update UI structure

---
+++ src/compenents/SearchDialog.tsx
@@ -0,0 +1,13 @@
+/**
+ * Custom modules
+ */
+import { APP, WEATHER_API } from "@/config/index";
+
+/**
+ * Hooks
+ */
+import { useEffect, useCallback, useState } from "react";
+
+/**
+ * Compenents
+ */
--- src/compenents/TopAppBar.tsx
+++ src/compenents/TopAppBar.tsx
@@ -4,6 +4,18 @@ import { ThemeProvider } from "./ThemeProvider"
  * Components
  */
 
+/**
+ * Assets
+ */
+import { Logo } from "@/assets/Logo";
+
 export const TopAppBar = () => {
-    return <div>Top app bar</div>;
+    return (
+        <div className='h-16 lg:my-4 outline outline-white'>
+            <header className='h-16 px-4 outline flex items-center justify-between fixed top-0 left-0 w-full bg-background/50 backdrop-blur-lg border-b z-50
+            lg:border lg:rounded-2xl lg:w-auto lg:max-w-384 lg:mx-auto lg:top-4 lg:left-4 lg:right-4'>
+                <Logo/>
+            </header>
+        </div>
+    )
 }
